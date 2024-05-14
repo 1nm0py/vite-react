54a783427574abc993108056e6bed87f764e3a1b
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-05-14 15:56:15 +0500

    Fix_02

--- src/components/End/Endl.jsx
+++ src/components/End/Endl.jsx
@@ -1,4 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap";
+import "./End.css"
 
 
 export default function endl() {
@@ -10,7 +11,8 @@ export default function endl() {
           <div className="librariy">
             <Row className="justify-content-space-between">
               <Col lg="12">
-                <h1 className="home_h1">Активность сообщества</h1>
+                <h1 className="end_h1">Активность сообщества</h1>
+                <p className="end_P">Фанатский и официальный контент для всех игр и программ в Steam. </p>
               </Col>
             </Row>
           </div>
